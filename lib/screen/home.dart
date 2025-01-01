1c7449225a4087f7999b44a4762077988cc92d80
Aayush <aayushtimsina19@gmail.com>
2025-01-01 19:34:50 +0530

    Created web

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -134,7 +134,7 @@ class _HomeState extends State<Home> {
                   "Analyse",
                   style: TextStyle(
                       color: Colors.black,
-                      fontSize: 15,
+                      fontSize: 16,
                       fontWeight: FontWeight.bold),
                 ),
               ),
