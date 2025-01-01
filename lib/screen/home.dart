fff7e4c823b99368d0f8019881201f0f02eb8668
Aayush <aayushtimsina19@gmail.com>
2025-01-01 19:42:01 +0530

    Update

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -27,145 +27,147 @@ class _HomeState extends State<Home> {
           ),
         ),
       ),
-      body: Container(
-        color: const Color.fromARGB(255, 58, 55, 55),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(top: 20),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          final bool isWideScreen = constraints.maxWidth > 600;
+          return SingleChildScrollView(
+            child: Container(
+              color: const Color.fromARGB(255, 58, 55, 55),
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  Row(
-                    children: [
-                      const Text(
-                        "Social media platform",
-                        style: TextStyle(
-                          fontSize: 15,
-                          color: Colors.white,
-                        ),
-                      ),
-                      const SizedBox(
-                        width: 10,
-                      ),
-                      DropdownButton<String>(
-                        dropdownColor: const Color.fromARGB(255, 58, 55, 55),
-                        value: selectedPlatform,
-                        items: [
-                          "Facebook",
-                          "Instagram",
-                          "X",
-                          "Linkedin",
-                        ].map((String platform) {
-                          return DropdownMenuItem<String>(
-                            value: platform,
-                            child: Text(
-                              platform,
-                              style: const TextStyle(
-                                color: Colors.green,
+                  Padding(
+                    padding: const EdgeInsets.only(top: 20),
+                    child: Wrap(
+                      alignment: WrapAlignment.center,
+                      spacing: 20,
+                      runSpacing: 10,
+                      children: [
+                        Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            const Text(
+                              "Social media platform",
+                              style: TextStyle(
+                                fontSize: 15,
+                                color: Colors.white,
                               ),
                             ),
-                          );
-                        }).toList(),
-                        onChanged: (String? newValue) {
-                          setState(() {
-                            selectedPlatform = newValue!;
-                          });
-                        },
-                      ),
-                    ],
+                            const SizedBox(width: 10),
+                            DropdownButton<String>(
+                              dropdownColor:
+                                  const Color.fromARGB(255, 58, 55, 55),
+                              value: selectedPlatform,
+                              items: [
+                                "Facebook",
+                                "Instagram",
+                                "X",
+                                "Linkedin",
+                              ].map((String platform) {
+                                return DropdownMenuItem<String>(
+                                  value: platform,
+                                  child: Text(
+                                    platform,
+                                    style: const TextStyle(
+                                      color: Colors.green,
+                                    ),
+                                  ),
+                                );
+                              }).toList(),
+                              onChanged: (String? newValue) {
+                                setState(() {
+                                  selectedPlatform = newValue!;
+                                });
+                              },
+                            ),
+                          ],
+                        ),
+                        Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            const Text(
+                              "Post type",
+                              style: TextStyle(
+                                fontSize: 15,
+                                color: Colors.white,
+                              ),
+                            ),
+                            const SizedBox(width: 10),
+                            DropdownButton<String>(
+                              dropdownColor:
+                                  const Color.fromARGB(255, 58, 55, 55),
+                              value: selectedPostType,
+                              items: [
+                                "Post",
+                                "Video",
+                                "Link",
+                              ].map((String postType) {
+                                return DropdownMenuItem<String>(
+                                  value: postType,
+                                  child: Text(
+                                    postType,
+                                    style: const TextStyle(
+                                      color: Colors.green,
+                                    ),
+                                  ),
+                                );
+                              }).toList(),
+                              onChanged: (String? newValue) {
+                                setState(() {
+                                  selectedPostType = newValue!;
+                                });
+                              },
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
                   ),
-                  const SizedBox(width: 20),
-                  Row(
-                    children: [
-                      const Text(
-                        "Post type",
+                  const SizedBox(height: 20),
+                  Container(
+                    width: isWideScreen ? 200 : 150,
+                    height: 40,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.white,
+                    ),
+                    child: TextButton(
+                      onPressed: () {},
+                      child: const Text(
+                        "Analyse",
                         style: TextStyle(
-                          fontSize: 15,
-                          color: Colors.white,
-                        ),
+                            color: Colors.black,
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold),
                       ),
-                      const SizedBox(
-                        width: 10,
-                      ),
-                      DropdownButton<String>(
-                        dropdownColor: const Color.fromARGB(255, 58, 55, 55),
-                        value: selectedPostType,
-                        items: [
-                          "Post",
-                          "Video",
-                          "Link",
-                        ].map((String postType) {
-                          return DropdownMenuItem<String>(
-                            value: postType,
-                            child: Text(
-                              postType,
-                              style: const TextStyle(
-                                color: Colors.green,
-                              ),
-                            ),
-                          );
-                        }).toList(),
-                        onChanged: (String? newValue) {
-                          setState(() {
-                            selectedPostType = newValue!;
-                          });
-                        },
-                      ),
-                    ],
+                    ),
+                  ),
+                  const Divider(color: Colors.black),
+                  const SizedBox(height: 10),
+                  const Text(
+                    "GPT Insights",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.green,
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  Container(
+                    height: 450,
+                    width: isWideScreen ? 600 : constraints.maxWidth * 0.9,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.grey,
+                    ),
                   ),
                 ],
               ),
             ),
-            const SizedBox(
-              height: 20,
-            ),
-            Container(
-              width: 150,
-              height: 40,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: Colors.white,
-              ),
-              child: TextButton(
-                onPressed: () {},
-                child: const Text(
-                  "Analyse",
-                  style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold),
-                ),
-              ),
-            ),
-            const Divider(
-              color: Colors.black,
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            const Text(
-              "GPT Insights",
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-                color: Colors.green,
-              ),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            Container(
-              height: 450,
-              width: 600,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: Colors.grey,
-              ),
-            )
-          ],
-        ),
+          );
+        },
       ),
     );
   }
